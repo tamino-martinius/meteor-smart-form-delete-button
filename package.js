1e92b26904d4df9fb99a80da7e9c9111a03ceb09
Tamino Martinius <dev@zaku.eu>
2016-02-19 16:04:28 +0100

    Fixed package url

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "zaku:smart-form-delete-button",
   version: "1.0.0",
   summary: "Provides a delete button UI component for smart-form",
-  git: "https://github.com/aldeed/meteor-smart-form-delete-button.git"
+  git: "https://github.com/tamino-martinius/meteor-smart-form-delete-button.git"
 });
 
 Package.onUse(function(api) {
